fc4d20710c37616fb148b3c12035324d076f4582
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-02 10:02:58 +0530

    changes saved

--- src/components/App.js
+++ src/components/App.js
@@ -5,80 +5,84 @@ const App = () => {
     const[input, setInput] = useState("");
 
     function Evaluate(){
-      setInput(eval(input));
+      try {
+      const result = eval(input);
+      setInput(String(result));
+    } catch (error) {
+      setInput("Error");
+    }
     }
   return (
-    <div clasName='calculator'>
+    <div clasName='Calculator'>
         <div className='input'>
             <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
         </div>
         <div className='buttons'>
-            <button className='btn-clear' onClick={(e)=>{
-              let val = input+e.target.innerText;
-              setInput(val);
+            <button id='btn-clear' onClick={()=>{
+              setInput('');
             }}>C</button>
-            <button className='btn-div' onClick={(e)=>{
+            <button id='btn-div' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>/</button>
-            <button className='btn-mult' onClick={(e)=>{
+            <button id='btn-mult' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>*</button>
-            <button className='btn-sub' onClick={(e)=>{
+            <button id='btn-sub' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>-</button>
-            <button className='btn-seven' onClick={(e)=>{
+            <button id='btn-7' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>7</button>
-            <button className='btn-oneight' onClick={(e)=>{
+            <button id='btn-8' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>8</button>
-            <button className='btn-nine' onClick={(e)=>{
+            <button id='btn-9' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>9</button>
-            <button className='btn-plus' onClick={(e)=>{
+            <button id='btn-plus' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>+</button>
-            <button className='btn-four' onClick={(e)=>{
+            <button id='btn-4' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>4</button>
-            <button className='btn-five' onClick={(e)=>{
+            <button id='btn-5' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>5</button>
-            <button className='btn-six' onClick={(e)=>{
+            <button id='btn-6' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>6</button>
-            <button className='btn-one' onClick={(e)=>{
+            <button id='btn-1' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>1</button>
-            <button className='btn-two' onClick={(e)=>{
+            <button id='btn-2' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>2</button>
-            <button className='btn-three' onClick={(e)=>{
+            <button id='btn-3' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>3</button>
-            <button className='btn-equal' onClick={Evaluate}>=</button>
-            <button className='btn-zero' onClick={(e)=>{
+            <button id='btn-equal' onClick={Evaluate}>=</button>
+            <button id='btn-0' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>0</button>
-            <button className='btn-dot' onClick={(e)=>{
+            <button id='btn-dot' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}>.</button>
-            <button className='btn-space' onClick={(e)=>{
+            <button id='btn-space' onClick={(e)=>{
               let val = input+e.target.innerText;
               setInput(val);
             }}></button>
